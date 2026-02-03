73f5e477a714ec5de60d9f0625b9fb080caa5e68
sidhigrover <sidhigrover29@gmail.com>
2026-02-03 11:18:43 +0530

    Alumni Section

--- src/components/Alumni.jsx
+++ src/components/Alumni.jsx
@@ -1,9 +1,52 @@
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import { AutoScroll } from "@splidejs/splide-extension-auto-scroll";
+import "@splidejs/react-splide/css";
+import "../ui/Alumni.css";
+import {alumniData} from "../data/alumni.js";
+
+
+
+
 const Alumni = () => {
-    return (
-      <div>
-      Alumni
-      </div>
-    );
+   return (
+    <section className="alumni-section">
+         <h1 className="alumni-title">
+      Voices From Our Community
+      </h1>
+
+      <Splide
+        options={{
+          type: "loop",
+          drag: "free",
+          arrows: false,
+          pagination: false,
+          gap: "2rem",
+          autoWidth: true,
+          focus: "center",
+          autoScroll: {
+            speed: 0.7,
+            pauseOnHover: false,
+          },
+        }}
+        extensions={{ AutoScroll }}
+      >
+        {alumniData.map((item, index) => (
+          <SplideSlide key={index}>
+            <div
+              className={`alumni-card ${item.theme}`}
+              style={{
+                transform: `rotate(${index % 2 === 0 ? "-2deg" : "1deg"})`,
+              }}
+            >
+              <p className="quote">“{item.quote}”</p>
+              <h4>{item.name}</h4>
+              <span>{item.role}</span>
+            </div>
+          </SplideSlide>
+        ))}
+      </Splide>
+    </section>
+  );
   };
   
   export default Alumni;
---
+++ src/data/alumni.js
@@ -0,0 +1,47 @@
+export const alumniData = [
+  {
+    quote:
+      "Codess cafe is a place of wholesome preparation for your interviews. From getting an opportunity to give mock interviews to having a personal review of your resume, you get every insight of interview preparation. It is a community where people share opportunities and help each other go. The mantra here is to ace interviews and out Mentor Aarnav helps us achieve it come what may!",
+    name: "Navpreet Kaur",
+    role: "⭐️⭐️⭐️⭐️⭐️",
+    theme: "light",
+  },
+  {
+    quote:
+      "Getting the right guidance at the right time is really important and I am grateful to get is from Aarnav Jindal under this program. He not only guided me but also took mock interviews which boosted my confidence and gave me a chance to analyse and improve and He always helped me in staying positive even after rejections. Thanks Aarnav and Codess.Cafe.",
+    name: "Prachi Singhal",
+    role: "⭐️⭐️⭐️⭐️⭐️",
+    theme: "red",
+  },
+  {
+    quote:
+      "My experience as a codess  mentee has been very enriching and positive. The program provided a valuable platform to interact with a diverse group of individuals, which helped me gain new perspectives . The mentor  played an important role by taking thoughtful initiatives to guide and support  throughout the journey. The consistent encouragement, clarity in guidance, and willingness to help made the learning process smooth and engaging. Overall, the experience was meaningful and contributed to both my personal and professional growth.",
+    name: "Sri Kavya Karri",
+    role: "⭐️⭐️⭐️⭐️⭐️",
+    theme: "light",
+  },
+  {
+    quote:
+      "If I had to best describe Codess.Cafe, I would say, I am a part of this small but thriving community of extremely smart women, who with the help of our talented mentor Aarnav Jindal are pushing our boundaries to become better at what we do. The idea is to throw away any inhibitions and doubts one has about herself, and look beyond what we've been told always by the society. The very streamlined nature of the material shared by Aarnav sir does exactly that! I am very grateful to be a part of this program.",
+    name: "Akanksha Tanwar",
+    role: "⭐️⭐️⭐️⭐️⭐️",
+    theme: "light",
+  },
+  {
+    quote:
+      "My experience at codess cafe has been really valuable so far. Getting insights on actual interview and placement scenarios + questions helps me in my journey. Mentors are active throughout and help resolve queries timely.",
+    name: "Tanishka Garg",
+    role: "⭐️⭐️⭐️⭐️⭐️",
+    theme: "light",
+  },
+  {
+    quote:
+      "My experience at Codess Cafe has been really inspiring and positive. It’s a supportive community where learning feels comfortable and motivating. The sessions are well-structured and practical, and I’ve gained both technical knowledge and confidence. Having a mentor makes the journey even better. They explain concepts clearly, encourage questions, and genuinely support growth. Overall, Codess Cafe feels like a safe and empowering space for learning and improving skills.",
+    name: "Suman Choudhury",
+    role: "⭐️⭐️⭐️⭐️⭐️",
+    theme: "red",
+  },
+
+
+];
+
